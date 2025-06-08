b51085931bcc92e2128105cfe5bdfb8bbdddac97
geeksamarth <samarthagrawal1804@gmail.com>
2025-06-09 00:54:01 +0530

    add: different models and utilities for api error and response

---
+++ src/models/task.models.js
@@ -0,0 +1,47 @@
+import mongoose, { Schema } from "mongoose";
+import { AvailableTaskStatus, TaskStatusEnum } from "../utils/constants.js";
+const taskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    project: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    assignedTo: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    assignedBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: AvailableTaskStatus,
+      default: TaskStatusEnum.TODO,
+    },
+    attachments: {
+      type: [
+        {
+          url: String,
+          mimetype: String,
+          size: Number,
+        },
+      ],
+      default: [],
+    },
+  },
+  { timestamps: true },
+);
+
+export const Task = mongoose.model("Task", taskSchema);
---
+++ src/models/user.models.js
@@ -0,0 +1,102 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+    },
+    avatar: {
+      type: {
+        url: String,
+        localpath: String,
+      },
+      default: {
+        url: `https://placehold.co/600x400`,
+        localpath: "",
+      },
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    refreshToken: {
+      type: String,
+    },
+    forgotPasswordToken: {
+      type: String,
+    },
+    forgotPasswordExpiry: {
+      type: Date,
+    },
+    emailVerificationToken: {
+      type: String,
+    },
+    emailVerificationExpiry: {
+      type: Date,
+    },
+  },
+  { timestamps: true },
+);
+
+// password hashing methods(hook)
+
+userSchema.pre("save", async function (next) {
+  if (!this.idModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// method to check if the password is correct or not
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// method to generate access token
+userSchema.methods.generateAccessToken = function () {
+  const accessToken = jwt.sign(
+    {
+      id: this._id,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY },
+  );
+  return accessToken;
+};
+
+// method to generate refresh token
+userSchema.methods.generateRefreshToken = function () {
+  const refreshToken = jwt.sign(
+    {
+      id: this._id,
+      email: this.email,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    },
+  );
+  return refreshToken;
+};
+
+export const User = mongoose.model("User", userSchema);
