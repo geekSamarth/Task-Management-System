f50ed826ee998daf37b843a86d98e1a4b10dd21b
geeksamarth <samarthagrawal1804@gmail.com>
2025-06-24 16:33:20 +0530

    add:some package, change readme file path, and configure various middleware

--- Backend/readme.md
+++
@@ -1,3 +0,0 @@
-# Task Management System
-
-### We are developing a task management system which have multi-features application purely based on managing the task efficiently.
--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,16 @@
 import express from "express";
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
 
 const app = express();
+app.use(express.json())
+app.use(cookieParser())
+app.use(express.urlencoded({extended:true}))
+app.use(cors({
+    credentials:true,
+    origin:['http://localhost:5173']
+}))
 
 // router imports
 
